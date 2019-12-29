9a586748145941de0b84f98339a61417ff79d1da
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-12-29 23:58:01 +0100

    improvement(frontend): add target _blank

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = ({ theme, toggleTheme }) => {
       <p>Built with <span role="img" aria-label="red heart">❤️</span> <i className="devicon-react-original colored"></i><span role="img" aria-label="styled components, wine and pizza"> 💅 🍷 and 🍕</span></p>
       <p id="switch" onClick={toggleTheme}>Switch to {theme === 'dark' ? 'light' : 'dark'} mode</p>
       <div>
-        <h5>Icons used courtesy of their respective authors from <a href="https://konpa.github.io/devicon/">Devicon,</a> <a href="https://iconscout.com/icons/">Iconscout,</a> <a href="https://www.flaticon.com/home">Flaticon</a> & <a href="https://commons.wikimedia.org/wiki/Main_Page">Commons</a>. </h5>
+        <h5>Icons used courtesy of their respective authors from <a href="https://konpa.github.io/devicon/" target="_blank" rel="noopener noreferrer">Devicon,</a> <a href="https://iconscout.com/icons/" target="_blank" rel="noopener noreferrer">Iconscout,</a> <a href="https://www.flaticon.com/home" target="_blank" rel="noopener noreferrer">Flaticon</a> & <a href="https://commons.wikimedia.org/wiki/Main_Page" target="_blank" rel="noopener noreferrer">Commons</a>. </h5>
         <h5>|</h5>
         <h5><a href="https://github.com/josenriagu/ubiquitous-tribble/issues/new/choose" target="_blank" rel="noopener noreferrer">Submit an issue</a></h5>
       </div>
