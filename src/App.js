b985a1279e89d79c334c6f973e93156cc71abdb4
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-12-29 14:17:47 +0100

    feat(frontend): create and implement contact section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Stack from './components/Stack';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import { AppDiv } from './App.styled';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <About />
         <Stack />
         <Projects />
+        <Contact />
       </AppDiv>
     </>
   );
